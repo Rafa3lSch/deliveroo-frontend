188e5323540b7d5d4cad723991644e04fd981199
Rafael Schiefelbein <80975453+Rafa3lSch@users.noreply.github.com>
2021-06-04 13:55:23 +0200

    front-back linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.css";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://deliveroo-backend-rs.herokuapp.com/"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <>
+      <div className="header">logo</div>
+      <div className="App">
+        {isLoading === true ? (
+          <p>En cours de chargement ...</p>
+        ) : (
+          <div>{data.restaurant.name}</div>
+        )}
+      </div>
+    </>
   );
 }
 
